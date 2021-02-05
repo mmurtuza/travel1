40ffd09fec13af8425491413e00ced9bb3b1fe7c
mmurtuza <49311438+mmurtuza@users.noreply.github.com>
2021-02-05 15:51:24 +0600

    3rd

--- app/Http/Controllers/EnterDataController.php
+++ app/Http/Controllers/EnterDataController.php
@@ -29,30 +29,77 @@ class EnterDataController extends Controller {
 
     public function create(Request $request){
 
-        $PAX= $request->input('PAX');
-        $SERVICE= $request->input('SERVICE');
-        $PNR= $request->input('PNR');
-        $P_P_NO= $request->input('P_P_NO');
-        $SECTOR= $request->input('SECTOR');
-        $AIR= $request->input('AIR');
-        $TKT_NO= $request->input('TKT_NO');
-        $VENDORE= $request->input('VENDOR');
-        $CLIENT = $request->input('CLIENTS');
-        $PAYABLE= $request->input('PAYABLE');
-        $PAID= $request->input('PAID');
-        $DUE = $PAYABLE - $PAID;
-        $RECEIVEABLE= $request->input('RECEIVEABLE');
-        $RECEIVED= $request->input('RECEIVED');
-        $TO_PAY = $RECEIVEABLE - $RECEIVED;
-        $time =date("Y-m-d");
-
-        try{
-            $datas = DB::insert('insert into purchase_data (DATE, PAX, SERVICE, PNR, P_P_NO, SECTOR, AIR, TKT_NO, VENDOR, PAYABLE, PAID, DUE, CLIENT, RECEIVEABLE, RECEIVED, TO_PAY) 
-            values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$time, $PAX, $SERVICE, $PNR, $P_P_NO, $SECTOR, $AIR, $TKT_NO, $VENDORE, $PAYABLE, $PAID, $DUE, $CLIENT, $RECEIVEABLE, $RECEIVED, $TO_PAY]);
-            return redirect('insert')->with('status',"Insert successfully");
-        }catch(Exception $e){
-            return redirect('insert')->with('failed',"operation failed");
+        $rules= [
+            'PAX'=>'required|string|min:3|max:255',
+            'SERVICE'=>'required|string|min:3|max:255',
+            'PNR'=>'required|string|min:3|max:255',
+            'P_P_NO'=>'required|string',
+            'SECTOR'=>'required|string',
+            'AIR'=>'required|string',
+            'TKT_NO'=>'required|string',
+            'VENDOR'=>'required|string',
+            'CLIENTS'=>'required|string',
+            'PAYABLE'=>'required|int',
+            'PAID'=>'required|int',
+            'RECEIVEABLE'=>'required|int',
+            'RECEIVED'=>'required|int'
+        ];
+        $validator = Validator::make($request->all(),$rules);
+		if ($validator->fails()) {
+			return redirect('insert')
+			->withInput()
+			->withErrors($validator);
+		}else{
+            $PAX= $request->input('PAX');
+            $SERVICE= $request->input('SERVICE');
+            $PNR= $request->input('PNR');
+            $P_P_NO= $request->input('P_P_NO');
+            $SECTOR= $request->input('SECTOR');
+            $AIR= $request->input('AIR');
+            $TKT_NO= $request->input('TKT_NO');
+            $VENDORE= $request->input('VENDOR');
+            $CLIENT = $request->input('CLIENTS');
+            $PAYABLE= $request->input('PAYABLE');
+            $PAID= $request->input('PAID');
+            $DUE = $PAYABLE - $PAID;
+            $RECEIVEABLE= $request->input('RECEIVEABLE');
+            $RECEIVED= $request->input('RECEIVED');
+            $TO_PAY = $RECEIVEABLE - $RECEIVED;
+            $time =date("Y-m-d");
+            
+            try{
+                $datas = DB::insert('insert into purchase_data (DATE, PAX, SERVICE, PNR, P_P_NO, SECTOR, AIR, TKT_NO, VENDOR, PAYABLE, PAID, DUE, CLIENT, RECEIVEABLE, RECEIVED, TO_PAY) 
+                values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$time, $PAX, $SERVICE, $PNR, $P_P_NO, $SECTOR, $AIR, $TKT_NO, $VENDORE, $PAYABLE, $PAID, $DUE, $CLIENT, $RECEIVEABLE, $RECEIVED, $TO_PAY]);
+                return redirect('insert')->with('status',"Insert successfully");
+            }catch(Exception $e){
+                return redirect('insert')->with('failed',"operation failed");
+            }
         }
+
+        // $PAX= $request->input('PAX');
+        // $SERVICE= $request->input('SERVICE');
+        // $PNR= $request->input('PNR');
+        // $P_P_NO= $request->input('P_P_NO');
+        // $SECTOR= $request->input('SECTOR');
+        // $AIR= $request->input('AIR');
+        // $TKT_NO= $request->input('TKT_NO');
+        // $VENDORE= $request->input('VENDOR');
+        // $CLIENT = $request->input('CLIENTS');
+        // $PAYABLE= $request->input('PAYABLE');
+        // $PAID= $request->input('PAID');
+        // $DUE = $PAYABLE - $PAID;
+        // $RECEIVEABLE= $request->input('RECEIVEABLE');
+        // $RECEIVED= $request->input('RECEIVED');
+        // $TO_PAY = $RECEIVEABLE - $RECEIVED;
+        // $time =date("Y-m-d");
+
+        // try{
+        //     $datas = DB::insert('insert into purchase_data (DATE, PAX, SERVICE, PNR, P_P_NO, SECTOR, AIR, TKT_NO, VENDOR, PAYABLE, PAID, DUE, CLIENT, RECEIVEABLE, RECEIVED, TO_PAY) 
+        //     values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$time, $PAX, $SERVICE, $PNR, $P_P_NO, $SECTOR, $AIR, $TKT_NO, $VENDORE, $PAYABLE, $PAID, $DUE, $CLIENT, $RECEIVEABLE, $RECEIVED, $TO_PAY]);
+        //     return redirect('insert')->with('status',"Insert successfully");
+        // }catch(Exception $e){
+        //     return redirect('insert')->with('failed',"operation failed");
+        // }
 
         
         // $rules = [
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,34 +16,38 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    
+
     <link rel="shortcut icon" href="{{ asset('../css/Favicon.png') }}">
     <!-- Styles -->
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
-      <!-- Bootstrap CSS -->
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <link href="{{ asset('../css/app.css') }}" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="{{ asset('../css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
-        
-        
-        
+
+
+
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            
+
             <div class="container" style="margin-left:25%">
-                
+
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        
+
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -54,22 +59,24 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-                            
+
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                            <li class="nav-item  ">
+                                <a id="navbarDropdown" class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ __('Logout') }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                                                             document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -84,7 +91,7 @@
             </div>
         </nav>
 
-        <main classs = "py-4">
+        <main classs="py-4">
             <div>
                 @yield('content')
             </div>
@@ -119,19 +126,27 @@
             
         </main> --}}
     </div>
-     <!-- Optional JavaScript -->
+    <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
     <script>
         function w3_open() {
-          document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("mySidebar").style.display = "block";
         }
-        
+
         function w3_close() {
-          document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("mySidebar").style.display = "none";
         }
-        </script>
+
+    </script>
 </body>
+
 </html>
